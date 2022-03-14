93eed15b2f23e4dfd4de1ded4d9a9253b6d017e9
Harsh Motwani <harshmotwanimail@gmail.com>
2022-03-14 21:51:07 +0530

    First release version

--- source/properties.h
+++ source/properties.h
@@ -6,8 +6,8 @@
 
 extern bool PROG_RUNNING;
 
-const Version VERSION(1,0,0,Version::VersionType::DEV);
-const bool DEBUG = true;
+const Version VERSION(1,0,0,Version::VersionType::RELEASE);
+const bool DEBUG = false;
 const std::string DATABASE_DIRECTORY = "/Users/harshmotwani/RDBMS/penguin_db/databases/";
 const std::string QUERY_DIRECTORY = "/Users/harshmotwani/RDBMS/penguin_db/queries/";
 
